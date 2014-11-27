ecbdf89e07e305dc0e0bbdaa8ae07dffa9ffbf9e
Steven Blenkinsop <steven099@gmail.com>
2014-11-26 20:25:13 -0500

    Add tictactoe game state normalization.

--- tictactoe/comparison.go
+++ tictactoe/comparison.go
@@ -1,5 +1,43 @@
 package tictactoe
 
+func Normalize(s State) State {
+	norm, norm_hash := s, hash(s)
+
+	foldIntoNorm := func(next State) {
+		next_hash := hash(next)
+		if next_hash < norm_hash {
+			norm, norm_hash = next, next_hash
+		}
+	}
+
+	foldIntoNorm(transpose(s))
+
+	s_flip_v := flip_v(s)
+	foldIntoNorm(s_flip_v)
+	foldIntoNorm(transpose(s_flip_v))
+
+	s_flip_h := flip_h(s)
+	foldIntoNorm(s_flip_h)
+	foldIntoNorm(transpose(s_flip_h))
+
+	s_rot_180 := flip_h(s_flip_v)
+	foldIntoNorm(s_rot_180)
+	foldIntoNorm(transpose(s_rot_180))
+
+	return norm
+}
+
+func hash(s State) int {
+	accum := 0
+	for i := 0; i < 3; i++ {
+		for j := 0; j < 3; j++ {
+			v := s.Board[i][j] + 1
+			accum = accum*3 + int(v)
+		}
+	}
+	return accum
+}
+
 func Equivalent(a, b State) bool {
 	if a == b || a == transpose(b) {
 		return true
